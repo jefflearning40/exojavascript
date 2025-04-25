05784a5430092c1c364a5c3514b14f132b452302
ledeunf <JeanFrancois@gmail.com>
2025-04-25 15:42:57 +0200

    defi6 et 7 dans defi5

---
+++ defi5v3.js
@@ -0,0 +1,17 @@
+const message="Le JavaScript est super";
+let mot=message.slice(3,13);
+console.log(mot);
+// defi 6
+const phrase = "Je suis développeur web";
+const longueur = phrase.length;
+console.log(longueur); 
+const hello=" Bonjour le monde ! ";
+console.log(hello);
+const resultat=hello.trim();
+console.log(resultat);
+
+
+
+
+
+
